13fdf65e02df8463362a2636c734dcb0ce62a9ee
bekirugurr <bekirugur070@gmail.com>
2022-07-02 15:51:47 +0300

    gt updte dlte mthds for ctgry quiz quest ans added

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
-from .views import CategoryListView, QuizListView, QuestionListView
+from .views import CategoryListCreateView, QuizListCreateView, QuestionListCreateView,CategoryGetUpdateDeleteView, QuizGetUpdateDeleteView, QuestionGetUpdateDeleteView, AnswerGetUpdateDeleteView
 
 urlpatterns = [
-    path('', CategoryListView.as_view()),
-    path('<category>/', QuizListView.as_view()),
-    path('<category>/<title>/', QuestionListView.as_view()),
     path('nested_admin/', include('nested_admin.urls')),
+    path('', CategoryListCreateView.as_view()),
+    path('category_id/<int:id>/', CategoryGetUpdateDeleteView.as_view()),
+    path('<category>/', QuizListCreateView.as_view()),
+    path('quiz_id/<int:id>/', QuizGetUpdateDeleteView.as_view()),
+    path('question_id/<int:id>/', QuestionGetUpdateDeleteView.as_view()), # iki alttaki satırdan aşağıda olursa çalışmıyor
+    path('answer_id/<int:id>/', AnswerGetUpdateDeleteView.as_view()), # alttaki satırdan aşağıda olursa çalışmıyor
+    path('<category>/<title>/', QuestionListCreateView.as_view()),
 
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, ListCreateAPIView
-from .models import Category, Quiz, Question
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Category, Quiz, Question, Answer
 from .serializers import CategorySerializer, QuizSerializer, QuestionSerializer, AnswerSerializer
 from .permissions import StafCUDAuthenticatedOnlyRead
 
-class CategoryListView(ListCreateAPIView):
+class CategoryListCreateView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (StafCUDAuthenticatedOnlyRead,)
 
-class QuizListView(ListCreateAPIView):
+class CategoryGetUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (StafCUDAuthenticatedOnlyRead,)
+    lookup_field = "id"
+
+
+class QuizListCreateView(ListCreateAPIView):
     serializer_class = QuizSerializer
     permission_classes = (StafCUDAuthenticatedOnlyRead,)
 
@@ -17,12 +24,34 @@ class QuizListView(ListCreateAPIView):
         category_name = self.kwargs['category']
         return Quiz.objects.filter(category__name__iexact = category_name)
 
-class QuestionListView(ListCreateAPIView):
+class QuizGetUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+    permission_classes = (StafCUDAuthenticatedOnlyRead,)
+    lookup_field = "id"
+
+
+class QuestionListCreateView(ListCreateAPIView):
     serializer_class = QuestionSerializer
     permission_classes = (StafCUDAuthenticatedOnlyRead,)
 
     def get_queryset(self):
         quiz_title = self.kwargs['title']
         return Question.objects.filter(quiz__title__iexact=quiz_title)
+
+class QuestionGetUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = (StafCUDAuthenticatedOnlyRead,)
+    lookup_field = "id"
+
+
+class AnswerGetUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = (StafCUDAuthenticatedOnlyRead,)
+    lookup_field = "id"
+
+
 
 
